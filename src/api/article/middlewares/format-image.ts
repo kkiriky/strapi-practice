87395a5743efdd80c65a68a66a381722e5e60fff
kkiriky <yoon_1139@naver.com>
2024-02-12 19:48:47 +0900

    refactor: entity, exception

--- src/api/article/middlewares/format-image.ts
+++ src/api/article/middlewares/format-image.ts
@@ -13,19 +13,15 @@ export default () => {
   return async (ctx: ExtendedContext, next: Next) => {
     await next();
 
-    // 존재하지 않는 route에 요청할 경우 (404) 대응
-    if (!ctx.body) return;
-    // is-owner 미들웨어에서 throw error 했을 경우 body에 data없이 error만 존재하므로 넘김
-    if (ctx.body.error) return;
-    if (!ctx.body.data) return;
-
-    if (Array.isArray(ctx.body.data)) {
-      // 응답 데이터가 list 형태일 경우 (ex. find)
-      ctx.body.data = ctx.body.data.map((article) => {
-        return plainToClass(ArticleEntity, article);
-      });
-    } else {
-      ctx.body.data = plainToClass(ArticleEntity, ctx.body.data);
+    if (ctx.body.data) {
+      if (Array.isArray(ctx.body.data)) {
+        // 응답 데이터가 list 형태일 경우 (ex. find)
+        ctx.body.data = ctx.body.data.map((article) => {
+          return plainToClass(ArticleEntity, article);
+        });
+      } else {
+        ctx.body.data = plainToClass(ArticleEntity, ctx.body.data);
+      }
     }
   };
 };
--- src/entities/article.entity.ts
+++ src/entities/article.entity.ts
@@ -1,22 +1,9 @@
-import { Attribute } from '@strapi/strapi';
 import { Exclude, Type } from 'class-transformer';
 import { AdminEntity } from './admin.entity';
 import { ImageEntity } from './image.entity';
 import { UserEntity } from './user.entity';
 
 export class ArticleEntity {
-  constructor(
-    partial:
-      | Attribute.GetValues<
-          'api::article.article',
-          | Attribute.GetNonPopulatableKeys<'api::article.article'>
-          | Attribute.GetPopulatableKeys<'api::article.article'>
-        >
-      | Partial<ArticleEntity>
-  ) {
-    Object.assign(this, partial);
-  }
-
   id: number;
   title: string;
   content: string;
